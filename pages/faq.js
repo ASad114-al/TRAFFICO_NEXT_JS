5449b20dd536760adb771e7c0e7e1af627120a59
Asad114-al <as3ad.94aa@gmail.com>
2020-12-09 00:21:13 +0100

    first push footer und navigation

---
+++ pages/faq.js
@@ -0,0 +1,12 @@
+import Headline from '../Componentes/Headline';
+// import Naviagtion from '../Componentes/Naviagation';
+import Layout from '../Componentes/Layout';
+const Faq = () => {
+    return (
+      <Layout>
+      <h1>hallo</h1>
+        </Layout>
+      );
+}
+ 
+export default Faq;
